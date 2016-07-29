858d6756563f29858fbe43508ddec13d43a688b1
Rémy MEJA <remy.meja@inist.fr>
2016-07-29 14:05:16 +0200

    fix orthograph

--- starter/walker-fs/walker-fs.js
+++ starter/walker-fs/walker-fs.js
@@ -15,7 +15,7 @@ class WalkerFS {
         'application/xml': ["nxml", "meta", "xlink_v03", "prime_v03", "plusxml_v02", "plusprime_v02", "info_V03", "citation_v03", "aux_v03"]
       });
       const data = {
-        extenstion: path.extname(stats.name),
+        extension: path.extname(stats.name),
         path: root + '/' + stats.name,
         mimetype: mime.lookup(root + '/' + stats.name),
         size: stats.size
