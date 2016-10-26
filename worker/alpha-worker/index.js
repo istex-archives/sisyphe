5e9e6e2bd1b0d4babe4fb915dd65f72ddfb88f0b
Rémy MEJA <remy.meja@inist.fr>
2016-10-26 15:32:24 +0200

    Shut it alpha-worker

--- worker/alpha-worker/index.js
+++ worker/alpha-worker/index.js
@@ -3,7 +3,7 @@
 class Business {
   doTheJob(data, next) {
     setTimeout(() => {
-      console.log('alpha-worker : ', data);
+      // console.log('alpha-worker : ', data);
       if (data.size > 5000 ) {
         next(new Error("I don't want yours bigfile, dude !"));
       } else {
