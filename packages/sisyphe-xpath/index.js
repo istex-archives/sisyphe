2e814620dccfe727e3222426352968289eb70854
Rémy MEJA <remy.meja@inist.fr>
2016-10-30 23:07:30 +0100

    reformating of code

--- packages/sisyphe-xpath/index.js
+++ packages/sisyphe-xpath/index.js
@@ -1,26 +1,28 @@
 'use strict';
 
 const redisHost = process.env.REDIS_HOST || 'localhost',
-      redisPort = process.env.REDIS_PORT || '6379';
+  redisPort = process.env.REDIS_PORT || '6379';
 
 const FromXml = require('xpath-generator').FromXml,
-      redis = require('redis'),
-      redisClient = redis.createClient(`//${redisHost}:${redisPort}`);
+  redis = require('redis'),
+  redisClient = redis.createClient(`//${redisHost}:${redisPort}`);
 
 
-function doTheJob(data,next) {
-  if(data.mimeType != 'application/xml'){
-    return next(null,data);
-  };
-  let xml = new FromXml().generate('./test.xml',true).then(result=>{
+const sisypheXpath = {};
+
+sisypheXpath.doTheJob = function (data, next) {
+  if (data.mimeType != 'application/xml') {
+    return next(null, data);
+  }
+  const xml = new FromXml();
+  xml.generate('./test.xml', true).then((result) => {
     data.xpaths = result;
-    next(null,data);
+    next(null, data);
   })
-}
+};
 
-function finalJob(){
+sisypheXpath.finalJob = function () {
   // When no more data in queue, sisyphe will execute it
-}
+};
 
-exports.doTheJob = doTheJob;
-exports.finalJob = finalJob;
+module.exports = sisypheXpath;
