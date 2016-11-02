e44d93d734b1bd281045795201027e240f39b9c1
matthd <matthias.dieudonne@gmail.com>
2016-11-02 16:07:04 +0100

    IncrementKey redis is ok

--- packages/sisyphe-xpath/index.js
+++ packages/sisyphe-xpath/index.js
@@ -6,18 +6,21 @@ const redisHost = process.env.REDIS_HOST || 'localhost',
 const FromXml = require('/Users/dieudonn/Documents/INIST/xpath-generator').FromXml,
   // FromXml = require('xpath-generator').FromXml,
   redis = require('redis'),
-  redisClient = redis.createClient(`//${redisHost}:${redisPort}`);
+  redisClient = redis.createClient(`//${redisHost}:${redisPort}`,{db : '1', prefix : 'sisyphe-xpath'});
 
-
-const sisypheXpath = {};
+const sisypheXpath = {},
+  xml = new FromXml();
 
 sisypheXpath.doTheJob = function (data, next) {
   if (data.mimetype != 'application/xml') {
     return next(null, data);
   }
-  const xml = new FromXml();
   xml.generate(data.path, true).then(result => {
     data.xpath = result;
+    let keys = Object.keys(result);
+    for(var i=0; i < keys.length; i++){
+      redisClient.incrby(keys[i],result[keys[i]].count);
+    }
     next(null, data);
   }).catch(err => {
     next(err);
