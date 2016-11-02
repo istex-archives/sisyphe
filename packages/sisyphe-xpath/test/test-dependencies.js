837cc81cadd9333b5256cdc85ab2ad5892c13e74
matthd <matthias.dieudonne@gmail.com>
2016-11-02 09:09:32 +0100

    Add test on xpath generated

--- packages/sisyphe-xpath/test/test-dependencies.js
+++ packages/sisyphe-xpath/test/test-dependencies.js
@@ -32,10 +32,10 @@ describe('Redis', () => {
 
 describe('DoTheJob', () => {
   it('Should not doing work if mimetype is not xml', (done) => {
-    var objTest = {
+    let objTest = {
       extension: '.xml',
-      path: 'test.pdf',
-      mimetype: 'application/pdf',
+      path: 'test/test.xxx',
+      mimetype: 'application/xxx',
       size: 500
     };
     doTheJob(objTest, (err, data)=> {
@@ -43,6 +43,25 @@ describe('DoTheJob', () => {
         return done(err);
       }
       expect(data).to.deep.equal(objTest);
+      done();
+    });
+  })
+  it('Should generate xpath obj', (done) => {
+    let objTest = {
+      extension: '.xml',
+      path: 'test/test.xml',
+      mimetype: 'application/xml',
+      size: 500
+    };
+    doTheJob(objTest, (err, data)=> {
+      if (err) {
+        return done(err);
+      }
+      expect(data.xpath).to.exist;
+      expect(data.xpath).to.be.an('object');
+      expect(data.xpath).to.be.not.empty;
+      expect(data.xpath[Object.keys(data.xpath)[0]]).to.have.property('foo')
+
       return done();
     });
   })
