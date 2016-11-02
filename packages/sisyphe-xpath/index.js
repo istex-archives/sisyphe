cdc2b325a321e3c31567230b97ea7a3c2140d2e4
matthd <matthias.dieudonne@gmail.com>
2016-11-02 17:05:46 +0100

    Start scan

--- packages/sisyphe-xpath/index.js
+++ packages/sisyphe-xpath/index.js
@@ -5,8 +5,10 @@ const redisHost = process.env.REDIS_HOST || 'localhost',
 
 const FromXml = require('/Users/dieudonn/Documents/INIST/xpath-generator').FromXml,
   // FromXml = require('xpath-generator').FromXml,
-  redis = require('redis'),
-  redisClient = redis.createClient(`//${redisHost}:${redisPort}`,{db : '1', prefix : 'sisyphe-xpath'});
+  bluebird = require('bluebird'),
+  redis = bluebird.promisifyAll(require('redis')),
+  redisClient = redis.createClient(`//${redisHost}:${redisPort}`,{db : '0'}),
+  redisScan = require('redisscan');
 
 const sisypheXpath = {},
   xml = new FromXml();
@@ -29,6 +31,29 @@ sisypheXpath.doTheJob = function (data, next) {
 
 sisypheXpath.finalJob = function () {
   // When no more data in queue, sisyphe will execute it
+  let myResults = new Set();
+  scanAsync('0', "*", myResults).map(myResults => { 
+    //console.log( myResults); 
+  });
 };
+
+function scanAsync(cursor, pattern, returnSet){
+  return redisClient.scanAsync(cursor, "MATCH", pattern, "COUNT", "100").then(reply => {
+    //console.log(reply)
+    cursor = reply[0];
+    var keys = reply[1];
+    keys.forEach(function(key,i){
+      redisClient.getAsync(key).then(function(res) {
+          console.log(res); 
+      });
+      returnSet.add(key);
+    });
+    if( cursor === '0' ){
+      return Array.from(returnSet);
+    }else{
+      return scanAsync(cursor, pattern, returnSet)
+    }
+  });
+}
 
 module.exports = sisypheXpath;
