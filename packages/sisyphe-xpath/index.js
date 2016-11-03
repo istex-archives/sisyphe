99fb3e4c558b532a94f794de43c940cdf4338cdd
matthd <matthias.dieudonne@gmail.com>
2016-11-03 17:53:27 +0100

    Work on the finalJob to create stream..

--- packages/sisyphe-xpath/index.js
+++ packages/sisyphe-xpath/index.js
@@ -5,9 +5,11 @@ const redisHost = process.env.REDIS_HOST || 'localhost',
 
 const FromXml = require('/Users/dieudonn/Documents/INIST/xpath-generator').FromXml,
   // FromXml = require('xpath-generator').FromXml,
-  bluebird = require('bluebird'),
-  redis = bluebird.promisifyAll(require('redis')),
-  redisClient = redis.createClient(`//${redisHost}:${redisPort}`,{db : '0'}),
+  Promise = require('bluebird'),
+  redis = Promise.promisifyAll(require('redis')),
+  fs = Promise.promisifyAll(require('fs')),
+  mkdirp = Promise.promisifyAll(require('mkdirp')),
+  redisClient = redis.createClient(`//${redisHost}:${redisPort}`,{db : '1'}),
   redisScan = require('redisscan');
 
 const sisypheXpath = {},
@@ -29,30 +31,50 @@ sisypheXpath.doTheJob = function (data, next) {
   })
 };
 
-sisypheXpath.finalJob = function () {
+sisypheXpath.finalJob = function (done) {
   // When no more data in queue, sisyphe will execute it
-  let myResults = new Set();
-  scanAsync('0', "*", myResults).map(myResults => { 
-    //console.log( myResults); 
+  mkdirp(`applis/istex/job/${Date.now()}`).then(()=>{
+    console.log('test')
+  })
+  .catch(err=>{
+    return done(err);
+  })
+  let xpathsStream = fs.createWriteStream(`/applis/istex/job/${Date.now()}/xpaths.txt`, {'flags': 'w'});
+  xpathsStream.on('error', function(err) {
+    return done(err);
+  });
+  scanAsync('0', '*').then((result)=>{
+    xpathsStream.write(result.toString());
+    console.log('result ',result)
+    done();
   });
 };
 
-function scanAsync(cursor, pattern, returnSet){
-  return redisClient.scanAsync(cursor, "MATCH", pattern, "COUNT", "100").then(reply => {
-    //console.log(reply)
+function scanAsync(cursor, pattern){
+  return redisClient.scanAsync(cursor, 'MATCH', pattern).then(reply => {
     cursor = reply[0];
-    var keys = reply[1];
-    keys.forEach(function(key,i){
-      redisClient.getAsync(key).then(function(res) {
-          console.log(res); 
-      });
-      returnSet.add(key);
-    });
-    if( cursor === '0' ){
-      return Array.from(returnSet);
-    }else{
-      return scanAsync(cursor, pattern, returnSet)
-    }
+    let keys = reply[1],
+        obj = {};
+
+    return Promise.map(keys,(key)=>{
+      return redisClient.getAsync(key);
+    }).then((results)=>{
+      for (var i = 0; i < keys.length; i++) {
+        obj[keys[i]] = results[i]
+      }
+      return obj;
+    })
+
+    //console.log([keys])
+    //console.log(reply)
+    // keys.forEach(function(key,i){
+    //   redisClient.getAsync(key).then(function(val) {
+    //     obj[key] = val;
+    //   });
+    // });
+    // redisClient.mgetAsync(keys).then(function(res) {
+    //   console.log(res); // => 'bar' 
+    // });
   });
 }
 
