0523b0cb3feac3d9cd45b8d0301b9bd09896b3b7
Rémy MEJA <remy.meja@inist.fr>
2016-11-04 19:23:56 +0100

    first commit

---
+++ packages/sisyphe-xml/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const DOMParser = require('xmldom').DOMParser,
+  bluebird = require('bluebird'),
+  fs = bluebird.promisifyAll(require('fs'));
+
+const sisypheXml = {};
+sisypheXml.doTheJob = function (data, next) {
+  if (data.mimetype === 'application/xml') {
+    const wellFormedError = {};
+    const errorHandle = function (wellFormedErrorObj) {
+      return function (level, msg, locator) {
+        wellFormedErrorObj[level] = {
+          message: msg,
+          locator
+        };
+      };
+    }
+
+    const parser = new DOMParser({
+      locator: {},
+      errorHandler: {
+        warning: errorHandle(wellFormedError),
+        error: errorHandle(wellFormedError),
+        fatalError: errorHandle(wellFormedError)
+      }
+    });
+
+    fs.readFileAsync(data.path, 'utf8').then((xmlContent) => {
+      parser.parseFromString(xmlContent, 'application/xml');
+      data.isWellFormed = Object.keys(wellFormedError).length === 0;
+      if (!data.isWellFormed) {
+        data.wellFormedError = wellFormedError;
+      }
+      if (data.name === '10.1177_030437548000600201.xml') console.log(data);
+      next(null, data);
+    }).catch((error) => {
+      next(error);
+    });
+  } else {
+    next(null, data);
+  }
+};
+
+module.exports = sisypheXml;
