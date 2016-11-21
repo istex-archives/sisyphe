55517a24eab4b45b103b5e45e20f2aed254343aa
Rémy MEJA <remy.meja@inist.fr>
2016-11-21 11:21:59 +0100

    Finish debugging not well-formed

--- packages/sisyphe-xml/index.js
+++ packages/sisyphe-xml/index.js
@@ -8,12 +8,10 @@ const DOMParser = require('xmldom').DOMParser,
 
 const sisypheXml = {};
 sisypheXml.doTheJob = function (data, next) {
-  
+
   if (data.mimetype !== 'application/xml') {
     return next(null, data);
   }
-
-  console.log(data.name)
 
   const wellFormedError = {};
   const errorHandle = function (wellFormedErrorObj) {
@@ -42,88 +40,73 @@ sisypheXml.doTheJob = function (data, next) {
       return JSON.parse(dataConf);
     });
   };
-  
-  fs.readFileAsync(data.path, 'utf8').then((xmlContent)=>{
+
+  fs.readFileAsync(data.path, 'utf8').then((xmlContent) => {
     const xmlDom = parser.parseFromString(xmlContent, 'application/xml');
     data.isWellFormed = Object.keys(wellFormedError).length === 0;
-    if (!data.isWellFormed) {
-      console.log('Not well formed', data.name)
+    if (data.isWellFormed) {
+      getDoctype.parseFileAsync(data.path).then((doctype) => {
+        data.doctype = doctype;
+        return getConf(data.corpusname);
+      }).then((conf) => {
+        if (!conf.metadata) {
+          // config file empty, add error here
+          next(null, data);
+          return;
+        }
+        conf.metadata.map((metadata) => {
+          // Select the first XPATH possibility
+          let value = null;
+          if (Array.isArray(metadata.xpath)) {
+            for (let i = 0; i < metadata.length; i++) {
+              let currValue = xpath.select(metadata[i].xpath, xmlDom);
+              if (currValue.length) {
+                value = currValue;
+                break;
+              }
+            }
+          } else {
+            value = xpath.select(metadata.xpath, xmlDom);
+          }
+          value = (metadata.type === 'String' || metadata.type === 'Number') ? value.toString() : value;
+
+          if (!value) {
+            // Will not check if value is empty
+            console.log('xpath :', metadata.xpath);
+            return;
+          }
+
+          if (!metadata.hasOwnProperty('regex')) {
+            value = (metadata.type === 'Number') ? parseInt(value, 10) : value;
+            data[metadata.name] = value;
+            return;
+          }
+
+          const regex = new RegExp(metadata.regex),
+            isValueValid = regex.test(value);
+
+          if (!isValueValid) {
+            data[metadata.name + 'IsValid'] = isValueValid;
+            data[metadata.name + 'Error'] = value;
+            return;
+          }
+          data[metadata.name + 'IsValid'] = isValueValid;
+          value = (metadata.type === 'Number') ? parseInt(value, 10) : value;
+          data[metadata.name] = value;
+        });
+        next(null, data);
+      }).catch(error => {
+        data.wellFormedError = error;
+        data.isWellFormed = false;
+        return next(null, data)
+      })
+    } else {
       data.wellFormedError = wellFormedError;
+      next(null, data);
     }
-  }).then(doctype=>{
-    next(null,data)
-  }).catch(err=>{
-    next(err,data);
-  })
-
-  // Promise.join(
-  //   fs.readFileAsync(data.path, 'utf8'),
-  //   getDoctype.parseFileAsync(data.path),
-  //   function(xmlContent, doctype) {
-
-  //     const xmlDom = parser.parseFromString(xmlContent, 'application/xml');
-  //     data.doctype = doctype;
-  //     data.isWellFormed = Object.keys(wellFormedError).length === 0;
-
-  //     if (!data.isWellFormed) {
-  //       console.log('Not well formed', data.name)
-  //       data.wellFormedError = wellFormedError;
-  //       return next(null, data);
-  //     }
-  //     console.log('Well formed', data.name)
-
-  //     getConf(data.corpusname).then((conf) => {
-  //       if(!conf.metadata){
-  //         console.log('no config file stop here')
-  //         //config file empty, add error here
-  //         return next(null, data);
-  //       }
-  //       conf.metadata.map((metadata) => {
-  //         // Select the first XPATH possibility
-  //         let value = null;
-  //         for (let i = 0; i < metadata.length; i++) {
-  //           let currValue = xpath.select(metadata[i].xpath, xmlDom);
-  //           if(currValue.length){
-  //             value = currValue;
-  //             break;
-  //           }
-  //         }
-
-  //         if(!value){
-  //           // Will not check if value is empty
-  //           return;
-  //         }
-  //         value = (metadata.type === 'String' || metadata.type === 'Number') ? 
-  //                 value = value.toString() : value;
-
-  //         if(!metadata.hasOwnProperty('regex')){
-  //           value = (metadata.type === 'Number') ? parseInt(value, 10) : value;
-  //           data[metadata.name] = value;
-  //           return;
-  //         }
-
-  //         const regex = new RegExp(metadata.regex),
-  //               isValueValid = regex.test(value);
-
-  //         if (!isValueValid) {
-  //           data[metadata.name + 'IsValid'] = isValueValid;
-  //           data[metadata.name + 'Error'] = value;
-  //           return;
-  //         }
-  //         data[metadata.name + 'IsValid'] = isValueValid;
-  //         value = (metadata.type === 'Number') ? parseInt(value, 10) : value;
-  //         data[metadata.name] = value;
-  //       });
-  //       next(null, data);
-  //     }).catch(error=>{
-  //       data.errorXPath = error;
-  //       return next(null,data)
-  //     })
-  //   }
-  // ).catch((error) => {
-  //   next(error);
-  // });
-  
+  }).catch((err) => {
+    next(err, data);
+  });
 };
 
 module.exports = sisypheXml;
