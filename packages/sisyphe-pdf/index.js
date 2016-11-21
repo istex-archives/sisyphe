5517c1535fd3082dd0b5cabdc09ccf6ea9e11ce8
Rémy MEJA <remy.meja@inist.fr>
2016-11-21 14:08:08 +0100

    Add some tests

--- packages/sisyphe-pdf/index.js
+++ packages/sisyphe-pdf/index.js
@@ -8,38 +8,7 @@ global.DOMParser = require('xmldom').DOMParser;
 const sisyphePdf = {};
 sisyphePdf.doTheJob = function (data, next) {
   if (data.mimetype === 'application/pdf') {
-
-    const getPdfWordCount = fs.readFileAsync(data.path).then((data) => {
-      const pdfData = new Uint8Array(data);
-      return PDFJS.getDocument(pdfData);
-    }).then((doc) => {
-      const numPages = doc.numPages;
-      data.pdfPageTotal = numPages;
-      const numPagesArr = Array.from(new Array(numPages), (val, index)=>index + 1);
-      return bluebird.map(numPagesArr, (pageNum) => {
-        return doc.getPage(pageNum)
-      })
-    }).then((pages) => {
-      return bluebird.map(pages, (page) => {
-        return page.getTextContent()
-      })
-    }).then((pagesContent) => {
-      return pagesContent.map((pageContent) => {
-        const strings = pageContent.items.map(function (item) {
-          return item.str;
-        });
-        return strings.join('').split(/\s+/).length;
-      }).reduce((previous, current) => previous + current)
-    });
-
-    const getPdfMetaData = fs.readFileAsync(data.path).then((data) => {
-      const pdfData = new Uint8Array(data);
-      return PDFJS.getDocument(pdfData);
-    }).then((doc) => {
-      return doc.getMetadata()
-    });
-
-    bluebird.join(getPdfMetaData, getPdfWordCount, (pdfMetadata, pdfWordCount) => {
+    bluebird.join(this.getPdfMetaData(data), this.getPdfWordCount(data), (pdfMetadata, pdfWordCount) => {
       data.pdfWordCount = pdfWordCount;
       data.pdfWordByPage = Math.floor(data.pdfWordCount / data.pdfPageTotal);
       data.pdfMetadata = pdfMetadata.info;
@@ -51,5 +20,42 @@ sisyphePdf.doTheJob = function (data, next) {
     next(null, data);
   }
 };
+
+sisyphePdf.getPdfMetaData = function (data) {
+  return fs.readFileAsync(data.path).then((data) => {
+    const pdfData = new Uint8Array(data);
+    return PDFJS.getDocument(pdfData);
+  }).then((doc) => {
+    return doc.getMetadata()
+  });
+};
+
+
+sisyphePdf.getPdfWordCount = function (data) {
+  return fs.readFileAsync(data.path).then((data) => {
+    const pdfData = new Uint8Array(data);
+    return PDFJS.getDocument(pdfData);
+  }).then((doc) => {
+    const numPages = doc.numPages;
+    data.pdfPageTotal = numPages;
+    const numPagesArr = Array.from(new Array(numPages), (val, index)=>index + 1);
+    return bluebird.map(numPagesArr, (pageNum) => {
+      return doc.getPage(pageNum)
+    })
+  }).then((pages) => {
+    return bluebird.map(pages, (page) => {
+      return page.getTextContent()
+    })
+  }).then((pagesContent) => {
+    return pagesContent.map((pageContent) => {
+      const strings = pageContent.items.map(function (item) {
+        return item.str;
+      });
+      return strings.join('').split(/\s+/).length;
+    }).reduce((previous, current) => previous + current)
+  });
+};
+
+
 
 module.exports = sisyphePdf;
