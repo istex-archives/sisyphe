aeaaeb1f23d7cbb54af84cf654ab66291d7f078e
MatthD <matthias.dieudonne@gmail.com>
2017-01-10 16:22:38 +0100

    Dont add wpath to ES doc

--- packages/sisyphe-xpath/index.js
+++ packages/sisyphe-xpath/index.js
@@ -27,7 +27,7 @@ sisypheXpath.doTheJob = function (data, next) {
   }
   // console.log('Not Ignored: ', data.name);
   xml.generate(data.path, true).then(result => {
-    data.xpath = result;
+    //data.xpath = result;
     let keys = Object.keys(result);
     for (let i = 0; i < keys.length; i++) {
       redisClient.incrby(keys[i], result[keys[i]].count);
