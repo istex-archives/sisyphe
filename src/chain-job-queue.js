ffceb3d9c88fb2e9bb1d2ead3c832d42dfe59ee0
MatthD <matthias.dieudonne@gmail.com>
2017-02-17 14:49:33 +0100

    Add more tiemout & Add sisyphe dashboard progression

--- src/chain-job-queue.js
+++ src/chain-job-queue.js
@@ -7,6 +7,7 @@ const Queue = require('bull'),
   winston = require('winston'),
   debounce = require('lodash/debounce'),
   throttle = require('lodash/throttle'),
+  blessed = require('blessed'),
   clientRedis = redis.createClient();
 
 const logger = new (winston.Logger)({
@@ -20,6 +21,78 @@ const logger = new (winston.Logger)({
     })
   ]
 });
+
+const screen = blessed.screen({
+  smartCSR: true
+});
+
+ // exit the program by using esc q or ctl-c
+screen.key(['escape', 'q', 'C-c'], (ch, key) =>{
+  return process.exit(0);
+});
+
+const textProgress = blessed.box({
+  top: 'center',
+  left: 'center',
+  width: '20%',
+  align: 'center',
+  height: 6,
+  content: 'Sisyphe is starting ...',
+  tags: true,
+  style: {
+    fg: 'white',
+    border: {
+      fg: '#ffffff'
+    },
+    hover: {
+      bg: 'green'
+    }
+  }
+});
+
+const bar = blessed.progressbar({
+  parent: screen,
+  border: 'line',
+  style: {
+    fg: 'green', bg: 'default',
+    bar: {bg: 'green', fg: 'blue'},
+    border: { fg: 'default', bg: 'default'}
+  },
+  pch: ' ',
+  top: 8,
+  left: 'center',
+  width: '50%',
+  height: 4,
+  filled: 0
+});
+
+const tableProgress = blessed.table({
+  top: 20,
+  left: 'center',
+  width: '50%',
+  align: 'center',
+  height: 10,
+  tags: true,
+  noCellBorders : false,
+  border: 'dashed',
+  fillCellBorders: true,
+  style: {
+    cell : {
+      fg: 'white',
+      border: { fg: 'default', bg: 'default'}
+    },
+    fg: 'white',
+    border: { fg: 'default', bg: 'default'}
+  }
+});
+
+screen.append(textProgress);
+screen.append(bar);
+screen.append(tableProgress);
+screen.render();
+  
+screen.title = 'Sisyphe progression dashboard';
+
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
@@ -75,7 +148,25 @@ class ChainJobQueue {
       });
 
       worker.queue.on('completed', (job, result) => {
-        process.stdout.write(kuler('-', 'green'));
+        //process.stdout.write(kuler('-', 'green'));
+        let totalGeneratedTask, totalPerformedTask;
+
+        clientRedis.hgetAsync('sisyphe','totalGeneratedTask').then(val=>{
+          totalGeneratedTask = val;
+          return clientRedis.hgetAsync('sisyphe','totalPerformedTask');
+        }).then(val=>{
+          totalPerformedTask = val || 0;
+          return clientRedis.hgetAsync('sisyphe','totalFailedTask');
+        }).then(totalFailedTask=>{
+          totalFailedTask = totalFailedTask || 0;
+          let progress = (totalPerformedTask/totalGeneratedTask)*100;
+          bar.setProgress(progress);
+          textProgress.setContent(`~ ${progress.toFixed(2)}%`)
+          tableProgress.setData([['{yellow-fg}totalGeneratedTask{/}', totalGeneratedTask],
+            ['{green-fg}totalPerformedTask{/}', totalPerformedTask],
+            ['{red-fg}totalFailedTask{/}', totalFailedTask]])
+          screen.render();
+        })
         const isTheLastWorker = listWorker.length === (index + 1);
         if (isTheLastWorker) {
           worker.totalPerformedTask++;
@@ -83,7 +174,7 @@ class ChainJobQueue {
           worker.totalPerformedTask = 0;
         } else {
           const workerAfter = listWorker[index + 1];
-          workerAfter.queue.add(result, {removeOnComplete: true, timeout: 60000});
+          workerAfter.queue.add(result, {removeOnComplete: true, timeout: 300000});
         }
       });
 
@@ -101,7 +192,7 @@ class ChainJobQueue {
   }
 
   addTask(task) {
-    this.listWorker[0].queue.add(task, {removeOnComplete: true, timeout: 60000});
+    this.listWorker[0].queue.add(task, {removeOnComplete: true, timeout: 300000});
     return this;
   }
 }
