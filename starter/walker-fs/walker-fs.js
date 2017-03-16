09b6943d1d9dc996ff32f6f30161581cbba7375e
Rémy MEJA <remy.meja@inist.fr>
2017-03-16 08:21:50 +0100

    Adding a hash to detect duplicates

--- starter/walker-fs/walker-fs.js
+++ starter/walker-fs/walker-fs.js
@@ -3,26 +3,27 @@
 const walk = require('walk'),
   path = require('path'),
   fs = require('fs'),
+  crypto = require('crypto'),
   mime = require('mime');
 
 class WalkerFS {
   constructor(options) {
     this._path = options.path;
     this.corpusname = options.corpusname;
-    // Use an init function should be better
     this.totalFile = 0;
     this.now = Date.now();
     this.functionEventOnFile = (root, stats) => {
-      const data = {
+      const data = fs.readFileSync(path.resolve(root, stats.name));
+
+      return {
         corpusname: this.corpusname,
         startAt: this.now,
+        hash: crypto.createHash('sha256').update(data).digest('hex'),
         extension: path.extname(stats.name),
-        path: path.resolve(root + '/' + stats.name),
+        path: path.resolve(root, stats.name),
         name: stats.name,
         size: stats.size
       };
-
-      return data;
     };
 
     this.functionEventOnData = (data) => {
