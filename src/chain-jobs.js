e35e826c573a720318cfbcb1b0e1011e3ba72a9b
MatthD <matthias.dieudonne@gmail.com>
2017-07-11 09:37:18 +0200

    Remove redis kue for dothejob chain

--- src/chain-jobs.js
+++ src/chain-jobs.js
@@ -14,41 +14,39 @@ queue.on( 'error', function( err ) {
 // load tasks
 let task = [];
 
-function doTasks(taskNb,cb){
-  // Create process queue listener (1 worker-type max per thread !!)
-  queue.process(`${workers[taskNb].name}${process.env.WORKER_ID}`, 1, function (job, done) {
-    job.data.info.processorNumber = process.env.WORKER_ID;
-    task[taskNb].doTheJob(job.data, function (err, data) {
-      if(err){
-        process.send({id: data.info.id, type: data.info.type, processedFiles: true, error: err});
-        return;
-      }
-      // Send info to master to increment data
-      workers[data.info.id].processedFiles++;
-      sendInfo();
-      // There are more worker to do with this job process
-      if(data.info.id < workers.length-1){
-        data.info.id++;
-        data.info.type = workers[data.info.id].name;
-        queue.create(`${workers[data.info.id].name}${process.env.WORKER_ID}`, data).removeOnComplete( true ).save();
-      }
-      done();
-    });
-  });
-}
-
 //create task listener
 for(let i = 0; i < workers.length; i++){
   task[i] = require(path.resolve(__dirname, '../', 'worker', workers[i].module));
-}
-
-// Now listen to all new job task & do them
-for(let i = 0; i < workers.length; i++){
-  //Init the task
   if(task[i].init){
     task[i].init(workers[i].options);
   }
-  doTasks(i);
+}
+
+// Create process queue listener (1 worker-type max per thread !!)
+queue.process(`${workers[0].name}${process.env.WORKER_ID}`, 1, function (job, done) {
+  let taskNb = 0;
+  job.data.info.processorNumber = process.env.WORKER_ID;
+  launchJob(job.data,taskNb,done);
+});
+
+function launchJob(data,taskNb,done) {
+  task[taskNb].doTheJob(data, function (err, data) {
+    if(err){
+      process.send({id: data.info.id, type: data.info.type, processedFiles: true, error: err});
+      return;
+    }
+    // Send info to master to increment data
+    workers[data.info.id].processedFiles++;
+    sendInfo();
+    // There are more worker to do with this job process
+    if(data.info.id < workers.length-1){
+      data.info.id++;
+      data.info.type = workers[data.info.id].name;
+      launchJob(data,++taskNb,done);
+      return;
+    }
+    done();
+  });
 }
 
 let sendInfo = _.debounce(function () {
