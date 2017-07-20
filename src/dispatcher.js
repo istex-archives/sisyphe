02b4ba589eba99d65e0af8f8aa9de0e55d4c74cf
labroche <corentin.labroche@inist.fr>
2017-07-20 13:30:33 +0200

    fix done callback

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -6,14 +6,15 @@ Dispatcher.init = function (task, options) {
   this.waitingQueue = [];
   this.tasks = task;
   this.options = options;
+  this.overSeers = []
 };
 
 Dispatcher.addOverseer = function (overseer) {
   this.waitingQueue.push(overseer);
+  this.overSeers.push(overseer)
 };
 Dispatcher.getOverseer = function (done) {
   if (this.waitingQueue.length !== 0) return done(this.waitingQueue.shift());
-
   const checkOverseerIsAvailable = setInterval(() => {
     if (this.waitingQueue.length !== 0) {
       clearInterval(checkOverseerIsAvailable);
@@ -33,14 +34,15 @@ Dispatcher.start = function (end) {
 
   this.tasks.process((job, done) => {
     this.getOverseer((overseer) => {
-      overseer.send(job.data);
       overseer.once("message", (msg) => {
         if (msg.isDone) {
+          console.log('done');
           this.addOverseer(overseer);
           debouncedCount();
-          done();
         }
       });
+      overseer.send(job.data);
+      done();
     });
   });
 };
