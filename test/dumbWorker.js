40a9628ae5b356b84db62f8bfe5d034ceba9c0f3
Rémy MEJA <remy.meja@inist.fr>
2017-07-24 14:44:29 +0200

    delete obsolete worker

--- test/dumbWorker.js
+++
@@ -1,9 +0,0 @@
-process.on('message', (msg) => {
-  // i'm so dumb
-  const time = ~~(Math.random() * 100);
-  // if (msg.type === 'pdf') console.log(process.pid, msg, time);
-  setTimeout(() => {
-    msg.isDone = true;
-    process.send(msg);
-  }, time);
-});
