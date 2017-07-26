dcf1ad9aee9a4e199d3957c04f0a7646dfafdaef
Rémy MEJA <remy.meja@inist.fr>
2017-07-26 10:13:31 +0200

    update manufactory + tests

--- test/manufactory-test.js
+++ test/manufactory-test.js
@@ -3,6 +3,7 @@
 const pkg = require('../package.json');
 const chai = require('chai');
 const expect = chai.expect;
+const path = require('path');
 const Manufactory = require('../src/manufactory');
 
 describe(`${pkg.name}/src/manufactory.js`, function () {
@@ -27,18 +28,74 @@ describe(`${pkg.name}/src/manufactory.js`, function () {
   });
 
   describe('#createOverseersForDispatchers', function () {
-    this.timeout(5000);
     it('should create overseers for each dispatchers', function () {
       const enterprise = Object.create(Manufactory);
       return enterprise
         .init()
-        .addWorker('dumbWorker')
+        .addWorker('walker-fs')
         .addWorker('dumbWorker')
         .createDispatchers()
-        .createOverseersForDispatchers().then((result) => {
-          console.log(result);
-        }).catch((error) => {
-          console.log(error);
+        .createOverseersForDispatchers()
+        .then(results => {
+          expect(results).to.be.an('array');
+          expect(results).to.have.lengthOf(2);
+          results.map(result => {
+            expect(result).to.be.an('array');
+            expect(result).to.have.lengthOf(enterprise.numCPUs);
+          });
+          enterprise.dispatchers.map(dispatcher => {
+            expect(dispatcher.waitingQueue).to.have.lengthOf(enterprise.numCPUs);
+          });
+        });
+    });
+  });
+
+  describe('#bindDispatchers', function () {
+    it('should bind dispatchers between them', function () {
+      const enterprise = Object.create(Manufactory);
+      return enterprise
+        .init()
+        .addWorker('walker-fs')
+        .addWorker('dumbWorker')
+        .createDispatchers()
+        .createOverseersForDispatchers()
+        .then(() => {
+          enterprise.bindDispatchers();
+          enterprise.dispatchers.map((dispatcher, index, array) => {
+            const isLastDispatcher = array.length === index + 1;
+            if (isLastDispatcher) {
+              expect(dispatcher.listenerCount('result')).to.equal(0);
+            } else {
+              expect(dispatcher.listenerCount('result')).to.equal(1);
+            }
+          });
+        });
+    });
+  });
+
+  describe('#initializeWorkers', function () {
+    it('should initialize workers', function () {
+      const enterprise = Object.create(Manufactory);
+      return enterprise
+        .init()
+        .addWorker('walker-fs')
+        .addWorker('dumbWorker')
+        .initializeWorkers();
+    });
+  });
+
+  describe('#start', function () {
+    this.timeout(5000);
+    it('should start manufactory', function () {
+      const enterprise = Object.create(Manufactory);
+      const inputPath = path.join(__dirname, '/data');
+      return enterprise
+        .init({inputPath})
+        .addWorker('walker-fs')
+        .addWorker('dumbWorker')
+        .initializeWorkers()
+        .then(() => {
+          return enterprise.start();
         });
     });
   });
