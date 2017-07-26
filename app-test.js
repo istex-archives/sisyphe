b3b4134623bd1fa3f648ea8f2c257fe23285f4b4
Rémy MEJA <remy.meja@inist.fr>
2017-07-26 13:47:19 +0200

    update app-test

--- app-test.js
+++ app-test.js
@@ -1,7 +1,7 @@
 const Manufactory = require('./src/manufactory');
 
 const inputPath = '/home/meja/Data/dataset-xsmall/bmj/acupmed/acupmed-11/acupmed-11-1';
-const workers = ['walker-fs', 'filetype', 'xpath'];
+const workers = ['walker-fs', 'filetype'];
 // const workers = ['walker-fs', 'dumbWorker'];
 
 const enterprise = Object.create(Manufactory);
@@ -11,5 +11,10 @@ workers.map(worker => {
   enterprise.addWorker(worker);
 });
 enterprise.initializeWorkers().then(() => {
+  enterprise.dispatchers[1].on('result', (msg) => {
+    console.log(msg);
+  });
   return enterprise.start();
+}).then(() => {
+  console.log('stop !');
 });
