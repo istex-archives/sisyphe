ea4bfc949db92e1de9eef7fc4efb7c843165a172
Rémy MEJA <remy.meja@inist.fr>
2017-07-26 16:52:34 +0200

    update app-test

--- app-test.js
+++ app-test.js
@@ -1,20 +1,40 @@
+const program = require('commander');
+const pkg = require('./package.json');
+const path = require('path');
 const Manufactory = require('./src/manufactory');
+const numCPUs = require('os').cpus().length;
 
-const inputPath = '/home/meja/Data/dataset-xsmall/bmj/acupmed/acupmed-11/acupmed-11-1';
+program
+  .version(pkg.version)
+  .usage('[options] <path>')
+  .option('-n, --corpusname <name>', "Choose an identifier 's Name", 'default')
+  .parse(process.argv);
+
+// Corpusname is default, we stop here
+if (program.corpusname === 'default') {
+  program.outputHelp();
+  process.exit(0);
+}
+
+const inputPath = (program.args[0].charAt(0) === '/') ? program.args[0] : path.join(__dirname, program.args[0]);
+
 const workers = ['walker-fs', 'filetype'];
-// const workers = ['walker-fs', 'dumbWorker'];
-
 const enterprise = Object.create(Manufactory);
-
-enterprise.init({ inputPath });
+enterprise.init({ inputPath, numCPUs });
 workers.map(worker => {
   enterprise.addWorker(worker);
 });
-enterprise.initializeWorkers().then(() => {
-  enterprise.dispatchers[1].on('result', (msg) => {
-    console.log(msg);
+enterprise
+  .initializeWorkers()
+  .then((result) => {
+    enterprise.dispatchers[1].on('result', msg => {
+      console.log(msg);
+    });
+    return enterprise.start();
+  })
+  .then(() => {
+    console.log('stop !');
+  })
+  .catch((error) => {
+    console.log(error);
   });
-  return enterprise.start();
-}).then(() => {
-  console.log('stop !');
-});
