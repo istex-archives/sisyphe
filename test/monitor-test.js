e0d6c27cba2c83cef1c7e8c2c7f3657991a9cc6b
labroche <corentin.labroche@inist.fr>
2017-07-31 13:10:56 +0200

    update test for monitor.js

--- test/monitor-test.js
+++ test/monitor-test.js
@@ -6,60 +6,54 @@ const expect = chai.expect;
 const monitor = require('../src/monitor');
 const Queue = require('bull');
 
-describe(`${pkg.name}/src/monitor.js`, function () {
-  describe('#init', function () {
-    it('should be initialized successfully', function () {
-      const monitorTest = Object.create(monitor)
-      monitorTest.init({prefix:'bull', refresh: 1000})
+describe(`${pkg.name}/src/monitor.js`, function() {
+  describe('#init', function() {
+    it('should be initialized successfully', function() {
+      const monitorTest = new monitor()
+      monitorTest.init({
+        prefix: 'testMonitor',
+        refresh: 10,
+        silent: true
+      })
+
+      expect(monitorTest).to.be.an.instanceof(monitor)
       expect(monitorTest.refresh).to.be.a('number')
       expect(monitorTest.prefix).to.be.a('string')
-      expect(monitorTest.monitorController).to.be.an('object')
       expect(monitorTest.workers).to.be.an('array').to.be.empty
     });
   });
-  describe('#launch', function () {
-    it('should be initialized successfully', function () {
-      const monitorTest = Object.create(monitor)
-      monitorTest.init({prefix:'bull', refresh: 1000}).launch()
-
+  describe('#launch', function() {
+    it('should be launch successfully', function(done) {
+      let monitorTest = new monitor()
+      monitorTest = monitorTest.init({
+        prefix: 'testMonitor',
+        refresh: 10,
+        silent: true
+      }).launch()
+      expect(monitorTest.intervalLoop).to.be.an('object').to.have.ownPropertyDescriptor('_idleTimeout', {
+        value: 40,
+        writable: true,
+        enumerable: true,
+        configurable: true
+      })
+      done()
+    });
+  });
+  describe('#exit', function() {
+    it('timer should be broken', function(done) {
+      let monitorTest = new monitor()
+      monitorTest = monitorTest.init({
+        prefix: 'testMonitor',
+        refresh: 10,
+        silent: true
+      }).launch().exit()
+      expect(monitorTest.intervalLoop).to.be.an('object').to.have.ownPropertyDescriptor('_idleTimeout', {
+        value: -1,
+        writable: true,
+        enumerable: true,
+        configurable: true
+      })
+      done()
     });
   });
 });
-
-
-function loadQueue()
-  const prefix = 'sisyphe'
-  const filetype = new Queue("filetype", {
-    prefix
-  });
-  const xml = new Queue("xml", {
-    prefix
-  });
-  const walker = new Queue("walker-fs", {
-    prefix
-  });
-
-
-  const nbDocs = 800
-  for (var i = 0; i < nbDocs; i++) {
-    await walker.add({
-      id: ~~(Math.random() * 100)
-    });
-  }
-
-  for (var i = 0; i < nbDocs; i++) {
-    await filetype.add({
-      id: ~~(Math.random() * 100)
-    });
-  }
-  for (var i = 0; i < nbDocs; i++) {
-    await xml.add({
-      id: ~~(Math.random() * 100)
-    });
-  }
-  return process(xml).then(_ => {
-      log('xml')
-      return process(filetype)
-    }).then(_=>{
-      log('filetype')
-    })
