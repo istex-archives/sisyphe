a9a26e15074da000440c85f7abdc1d942ad4fc09
labroche <corentin.labroche@inist.fr>
2017-07-31 17:17:23 +0200

    break is never reach

--- src/monitor/monitorHelpers.js
+++ src/monitor/monitorHelpers.js
@@ -12,19 +12,14 @@ module.exports.getColorOfPercent = function(percent) {
   switch (true) {
     case percent >= 0 && percent <= 20:
       return color = 'red'
-      break;
     case percent > 20 && percent <= 40:
       return color = 'yellow'
-      break;
     case percent > 40 && percent <= 60:
       return color = 'magenta'
-      break;
     case percent > 60 && percent <= 80:
       return color = 'cyan'
-      break;
     case percent > 80 && percent <= 100:
       return color = 'green'
-      break;
     default:
       return color = 'red'
   }
