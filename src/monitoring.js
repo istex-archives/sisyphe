0ee2663d0fc953b8e0bc1e72780f7024cfa8e782
labroche <corentin.labroche@inist.fr>
2017-09-08 10:04:08 +0200

    update the monitoring

--- src/monitoring.js
+++ src/monitoring.js
@@ -1,5 +1,5 @@
-const bluebird = require('bluebird');
-const redis = require('redis');
+const bluebird = require("bluebird");
+const redis = require("redis");
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
@@ -9,27 +9,38 @@ const monitoring = {
     error: [],
     warning: [],
     info: []
-  },
-  workersError: {}
-};
-monitoring.updateLog = async function (type, string) {
-  if (type === 'error') {
-    const error = string.err;
-    if (string.hasOwnProperty('job') && string.job.hasOwnProperty('workerType')) {
-      if (!this.workersError[string.job.workerType]) this.workersError[string.job.workerType] = [];
-      this.workersError[string.job.workerType].push(string.job);
-    }
-    if (error.hasOwnProperty('stack')) string = error.message + ': ' + error.stack.split('\n')[1];
-    else string = '';
-    if (error.hasOwnProperty('infos') && Array.isArray(error.infos)) {
-      for (var i = 0; i < error.infos.length; i++) {
-        var info = error.infos[i];
-        string += '##' + info;
-      }
-    }
   }
+};
+monitoring.updateLog = async function(type, string) {
+  if (
+    string.hasOwnProperty('message') &&
+    string.hasOwnProperty('stack') ||
+    type === 'error'
+  ) return this.updateError(string)
+
   this.log[type].push(string);
-  await client.hsetAsync('monitoring', 'log', JSON.stringify(this.log), 'workersError', JSON.stringify(this.workersError));
+  await client.hsetAsync(
+    "monitoring",
+    "log",JSON.stringify(this.log)
+  );
+};
+
+monitoring.updateError = async function(err) {
+  if (
+    !(err.hasOwnProperty('message') && err.hasOwnProperty('stack')) 
+    && typeof String
+  ) {
+    err = new Error(err);
+    err.stack = undefined
+  } 
+
+  
+
+  this.log["error"].push(err);
+  await client.hsetAsync(
+    "monitoring",
+    "log", JSON.stringify(this.log)
+  );
 };
 
 module.exports = monitoring;
