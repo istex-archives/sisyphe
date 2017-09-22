f9606bd4e32641cb06251aa7b9100b6a60dd0294
labroche <corentin.labroche@inist.fr>
2017-09-22 09:10:00 +0200

    simplify the comman server

--- server.js
+++ server.js
@@ -6,47 +6,20 @@ const client = redis.createClient();
 const cp = require("child_process");
 const fs = Promise.promisifyAll(require("fs"));
 
-const sisyphePath = process.env.SISYPHE_PATH;
-if (!sisyphePath) {
-  console.log(
-    "Please set environnement variable SISYPHE_PATH to the sisyphe executable"
-  );
-  process.exit(0);
-} else {
-  if (fs.existsSync(sisyphePath)) {
-    launch();
-  } else {
-    console.log("Please verify if $SISYPHE_PATH is good");
-    process.exit(0);
-  }
-}
-
-async function launch() {
+function launch() {
   let retrieveData = true;
-  setInterval(_ => {
+  setInterval(async _ => {
     if (!retrieveData) return;
-    getCommand()
-      .then(data => {
-        if (data) {
-          retrieveData = false;
-          console.log('launch:', data)
-          return new Promise((resolve, reject) => {
-            const sisyphe = cp.exec(`./app ${data}`, (err, stdout, stderr) => {
-              retrieveData = true
-            });
-          });
-        }
-        else {
-          retrieveData=true
-        }
-      })
-      .catch((err, err1) => {
-        console.log(err);
-      });
-  }, 1000);
+    const command = await client.lpopAsync("command");
+    if (command) {
+      retrieveData = false;
+      console.log('launch:', command)
+      cp.exec(`./app ${command}`,_ =>retrieveData = true)
+    }
+    else {
+      retrieveData=true
+    }
+  }, 1000)
 }
 
-async function getCommand() {
-  let command = await client.lpopAsync("command");
-  return command;
-}
+launch()
