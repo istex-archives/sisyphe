11077610f967d0a9a36d4a06131cc8fe7199ffc0
labroche <corentin.labroche@inist.fr>
2017-10-26 16:01:24 +0200

    manage sisyphe version from asisyphe-monitor

--- server.js
+++ server.js
@@ -11,9 +11,65 @@ var app = express();
 app.use(serveStatic(path.join(__dirname, 'out')));
 app.use(bodyParser.json());
 
-app.get('/workers', function (req, res) {
-  const workers = require('./src/worker.json');
+app.get("/workers", function(req, res) {
+  const workers = require("./src/worker.json");
   res.json(workers);
+});
+
+
+app.get("/sisypheVersions", function(req, res) {
+  const listWorkers = require('./src/worker.json').workers
+  const modulesVersion = listWorkers.map(name=>{
+    return {
+      name,
+      version: require('./src/worker/' + name + '/package.json').version
+    }
+  })
+  res.status(200).json({
+    version: require('./package').version,
+    modules: modulesVersion
+  })
+});
+cp.exec('git status', (error,stderr,stdout)=>{
+  console.log(error,stdout,stderr)
+})
+
+app.get("/branches", function(req, res) {
+  cp.exec("git branch", (error, stdout, stderr) => {
+    const result = {};
+    result.branches = stdout
+      .split("\n")
+      .map(branch => {
+        if (branch.charAt(0) == "*"){
+          branch = branch.split(" ")[1];
+          result.currentBranch = branch
+        }
+        return branch.trim();
+      })
+      .filter(branch => branch !== "");
+    res.json(result);
+  });
+});
+
+
+app.post("/changeBranch", function(req, res) {
+  cp.exec("git checkout " + req.body.branch.trim(), (error, stdout, stderr) => {
+    if (error) res.status(500).json(error);
+    else if (stderr) res.status(500).json(stderr);
+    else if (stdout) res.status(200).json(stdout);
+  });
+});
+app.post("/pull", function(req, res) {
+  cp.exec("git pull", (error, stdout, stderr) => {
+    if (error) res.status(500).json(error);
+    else if (stderr) res.status(500).json(stderr);
+    else if (stdout) res.status(200).json(stdout);
+  });
+});
+app.get("/branchStatus", function(req, res) {
+  cp.exec("git status", (error, stdout, stderr) => {
+    res.json(stdout);
+  });
 });
 
 app.get('/download/latest', async function (req, res) {
