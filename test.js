14cc5ca635d2fac7ca1e9f26516708adf23bd56a
labroche <corentin.labroche@inist.fr>
2017-11-20 14:10:45 +0100

    remove unnecessary file

--- test.js
+++
@@ -1,7 +0,0 @@
-const array = [];
-const Promise = require("bluebird");
-const fs = require('fs-extra');
-(async _=>{
-  const a = await fs.readFile("./out/1510910578774-lavoisier/analyse-lavoisier.json", 'utf8');
-  
-})()
