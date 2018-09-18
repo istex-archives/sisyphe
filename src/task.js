cd46a4cf1431042ebfceed00d139cbb8ed8a9ae5
Rémy MEJA <remymeja@free.fr>
2018-09-18 08:35:25 +0200

    fix tasks number attempts

--- src/task.js
+++ src/task.js
@@ -25,21 +25,11 @@ Task.init = function (options) {
  * @returns Promise
  */
 Task.add = function (obj) {
-  return this.queue.add(obj, {removeOnComplete: true, attempts: 200000});
+  return this.queue.add(obj, {removeOnComplete: true, attempts: 5});
 };
 
 Task.process = async function (fun, queue) {
   return this.queue.process(fun);
-  // console.log('lkj')
-  // if (!this.queue) {
-  //   this.queue = queue
-  // }
-  // if (true) {
-  //   const job = await this.queue.getNextJob().catch(err =>{});
-  //   console.log(job)
-  //   if (job) await fun(job).catch(err=>{});
-  // }
-  // process.bind(this, fun, this.queue)()
 };
 Task.pause = async function () {
   return this.queue.pause();
